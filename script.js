0908e01cbc5697044c87e537148412ff1bc72759
Your Name <george.murgoci94@gmail.com>
2021-04-11 19:36:42 +0300

    fixed stuff

--- script.js
+++ script.js
@@ -176,26 +176,29 @@ const setValuesForCircle = () => {
 setValuesForCircle();
 pageScroll(0);
 
-const seeCertificates = () => {
+(function seeCertificates() {
   const certificateBtn = document.querySelector(".fa-file-alt");
   const certificatesContainer = document.querySelector(".diplomas-container");
   const certificates = document.querySelectorAll(".box>img");
   const boxes = document.querySelectorAll(".box");
-
+  const projectsContainer = document.querySelector(".experience-chapter");
   certificateBtn.addEventListener("click", () => {
-    certificatesContainer.style.display = "grid";
+    certificatesContainer.style.transform = "translateX(0px)";
+    setTimeout(() => {
+      projectsContainer.style.top = "150rem";
+    }, 1000);
 
     boxes.forEach((box) => {
       box.addEventListener("click", (event) => {
         event.stopPropagation();
         removeCertificateActiveClass();
         box.classList.add("active");
-        pageScroll(1800);
+        // pageScroll(1800);
       });
     });
+    pageScroll(1300);
   });
-};
-seeCertificates();
+})();
 
 const removeCertificateActiveClass = () => {
   const boxes = document.querySelectorAll(".box");
@@ -205,15 +208,21 @@ const removeCertificateActiveClass = () => {
   });
 };
 
-const hideCertificates = () => {
+(function hideCertificates() {
   const certificateContainer = document.querySelector(".diplomas-container");
+  const closeCertificationsBtn = certificateContainer.querySelector(
+    ".fa-angle-right"
+  );
+  const projectsContainer = document.querySelector(".experience-chapter");
 
-  certificateContainer.addEventListener("click", () => {
-    certificateContainer.style.display = "none";
+  closeCertificationsBtn.addEventListener("click", () => {
+    certificateContainer.style.transform = "translateX(400%)";
+    projectsContainer.style.top = "134rem";
+
     removeCertificateActiveClass();
   });
-};
-hideCertificates();
+})();
+
 // projects
 //movement animation
 
@@ -284,14 +293,14 @@ const addHoverEffect = () => {
 
   seeMoreBtn.style.transform = "translateZ(90px)";
   firstPhoto.style.transform = "translateZ(200px) ";
-  firstPhoto.style.transform = "rotate(-5deg)";
+  // firstPhoto.style.transform = "rotate(-5deg)";
   secondPhoto.style.transform = "translateZ(150px) ";
-  secondPhoto.style.transform = "rotate(-10deg)";
+  // secondPhoto.style.transform = "rotate(-10deg)";
   thirdPhoto.style.transform = "translateZ(100px) ";
-  thirdPhoto.style.transform = "rotate(-15deg)";
+  // thirdPhoto.style.transform = "rotate(-15deg)";
 
   fourthPhoto.style.transform = "translateZ(50px) ";
-  fourthPhoto.style.transform = "rotate(-20deg)";
+  // fourthPhoto.style.transform = "rotate(-20deg)";
 };
 const clickOnCardImages = () => {
   let trigger = true;
@@ -310,48 +319,60 @@ const clickOnCardImages = () => {
   });
 };
 //changeProject
-const changeProject = () => {
+const changeProject = (arr) => {
   const card = document.querySelector(".project-card");
-  setTimeout(insertNextProjects, 1000);
+  setTimeout(() => {
+    insertNextProjects(arr);
+  }, 500);
 
-  card.style.animation = "rotation 2s forwards";
+  card.style.animation = "rotation 1s forwards";
 };
-
+// event listeners for project card btns
 const clickRightBrn = () => {
-  const rightArrowBtn = document.querySelector(".fa-angle-right");
+  const projectsContainer = document.querySelector(".project-container");
+  const rightArrowBtn = projectsContainer.querySelector(".fa-angle-right");
   rightArrowBtn.addEventListener("click", () => {
-    changeProject();
+    changeProject(projectsArr);
   });
 };
-// clickRightBrn();
+const clickLeftBtn = () => {
+  const projectsContainer = document.querySelector(".project-container");
+  const rightArrowBtn = projectsContainer.querySelector(".fa-angle-left");
+  rightArrowBtn.addEventListener("click", () => {
+    changeProject(reverseProjectsArr);
+  });
+};
 
 // other projects arr
 const projectsArr = [
   {
     name: "Portofolio Website",
     paragraphCard:
-      "There is no surprise that my personal portofolio si done by .. well ..me. I included this in my my projects portofolio because I stumbled upon some interesting problems along the way and  the way I managed to fix the is.. at least interesting.",
+      "There is no surprise that my personal portofolio si done by .. well ..me. I included this here because I stumbled upon some interesting problems along the way and  the way I managed to fix the is... at least interesting.",
     picture1: `./resources/project-photos/Portofolio-project/quote.jpg`,
     picture2: `./resources/project-photos/Portofolio-project/level.jpg `,
     picture3: `./resources/project-photos/Portofolio-project/certifications.jpg`,
     picture4: `./resources/project-photos/Portofolio-project/project.jpg`,
     seeMoreContainer: [
       {
-        title: "ceva ca si titlu",
+        title: "Musicare",
+        projectDescriptionParagraph:
+          "ipsum dolor sit amet consectetur adipisicing elit. Fugiat sequi ipsum, magnam natus qui officia unde est possimus modi vel nobis voluptatum ab quis eius excepturi rerum exercitationem. Eos, ipsaipsum dolor sit amet consectetur adipisicing elit. Fugiat sequi ipsum, magnam natus qui officia unde est possimus modi vel nobis voluptatum ab quis eius excepturi rerum exercitationem. Eos, ipsa",
         paragraph1:
           " ipsum dolor sit amet consectetur adipisicing elit. Fugiat sequi ipsum, magnam natus qui officia unde est possimus modi vel nobis voluptatum ab quis eius excepturi rerum exercitationem. Eos, ipsa?",
-        subtitle: "chalanges",
-        paragraph2:
+        subtitle: "Chalanges",
+        subtitleParagraph:
           " ipsum dolor sit amet consectetur adipisicing elit. Fugiat sequi ipsum, magnam natus qui officia unde est possimus modi vel nobis voluptatum ab quis eius excepturi rerum exercitationem. Eos, ipsa?",
-        subtitle2: "How i Ovecome them",
-        paragraph3:
+        subtitle2: "How i Overcome them",
+        subtitleParagraph2:
           " ipsum dolor sit amet consectetur adipisicing elit. Fugiat sequi ipsum, magnam natus qui officia unde est possimus modi vel nobis voluptatum ab quis eius excepturi rerum exercitationem. Eos, ipsa?",
+
         bestFeatures: [
-          "fancy smancy design",
-          "storyTelling engine",
-          "cacat cu mac",
-          "altceva",
-          "inca ceva",
+          "Modern but elegant design",
+          "Dinamic accent color for the different sites that you choose",
+          "Split landing page",
+          "Working instrument filter",
+          "Secret quizz game for discounts",
         ],
       },
     ],
@@ -367,35 +388,137 @@ const projectsArr = [
     seeMoreContainer: [
       {
         title: "ceva ca si titlu",
+        projectDescriptionParagraph:
+          "ipsum dolor sit amet consectetur adipisicing elit. Fugiat sequi ipsum, magnam natus qui officia unde est possimus modi vel nobis voluptatum ab quis eius excepturi rerum exercitationem. Eos, ipsaipsum dolor sit amet consectetur adipisicing elit. Fugiat sequi ipsum, magnam natus qui officia unde est possimus modi vel nobis voluptatum ab quis eius excepturi rerum exercitationem. Eos, ipsa",
         paragraph1:
           " ipsum dolor sit amet consectetur adipisicing elit. Fugiat sequi ipsum, magnam natus qui officia unde est possimus modi vel nobis voluptatum ab quis eius excepturi rerum exercitationem. Eos, ipsa?",
         subtitle: "chalanges",
-        paragraph2:
+        subtitleParagraph:
           " ipsum dolor sit amet consectetur adipisicing elit. Fugiat sequi ipsum, magnam natus qui officia unde est possimus modi vel nobis voluptatum ab quis eius excepturi rerum exercitationem. Eos, ipsa?",
         subtitle2: "How i Ovecome them",
-        paragraph3:
+        subtitleParagraph2:
           " ipsum dolor sit amet consectetur adipisicing elit. Fugiat sequi ipsum, magnam natus qui officia unde est possimus modi vel nobis voluptatum ab quis eius excepturi rerum exercitationem. Eos, ipsa?",
         bestFeatures: [
-          "fancy smancy design",
-          "storyTelling engine",
-          "cacat cu mac",
-          "altceva",
-          "inca ceva",
+          "--Modern but elegant design",
+          "--Dinamic accent color for the different sites that you choose",
+          "--Split landing page",
+          "--Working instrument filter",
+          "--Secret quizz game for discounts",
         ],
+        moreProjectImages: {
+          pic1: `./resources/project-photos/musicare-project-image-slider.jpg`,
+          pic2: `./resources/project-photos/musicare-project-grid-photo.jpg`,
+          pic3: `./resources/project-photos/musicare-project-hero-photo.jpg`,
+          pic4: `./resources/project-photos/checkout-form.jpg`,
+        },
       },
     ],
   },
 ];
+const reverseProjectsArr = projectsArr.reverse();
 let projectIndex = 0;
 const projectNames = [];
 
-const insertNextProjects = () => {
+// //////////////////
+//insert feattureList
+
+const insertListElements = (element, projectFeatureList, counter) => {
+  const bestProjectFeatures = [...element.bestFeatures];
+  const featureArr = [];
+  bestProjectFeatures.forEach((feature) => {
+    if (feature !== undefined) {
+      if (counter < bestProjectFeatures.length) {
+        featureArr.push(feature);
+      } else return;
+    }
+    featureArr.forEach((feature) => {
+      const listEl = document.createElement("li");
+
+      listEl.innerText = feature;
+      projectFeatureList.insertAdjacentElement("beforeend", listEl);
+      counter++;
+      console.log(projectFeatureList);
+    });
+  });
+};
+const insertProjectDetails = (project, counter) => {
+  const projectDetailsContainer = document.querySelector(
+    ".project-details-container"
+  );
+  const projectDetailsPictureContainer = document.querySelector(
+    ".project-details-picture-grid"
+  );
+  const projectFeatureList = document.querySelector(".best-features");
+
+  project.seeMoreContainer.forEach((element) => {
+    //variables of project description object
+    let title = element.title;
+    let projectDescription = element.projectDescriptionParagraph;
+    let paragraph1 = element.paragraph1;
+    let subtitleParagraph = element.subtitleParagraph;
+    let subtitleParagraph2 = element.subtitleParagraph;
+    let subtitle1 = element.subtitle1;
+    let subtitle2 = element.subtitle2;
+    insertListElements(element, projectFeatureList, counter);
+    //insertHtml
+    const projectDetailsHTML = `
+      <h1 class="project-title">${title}</h1>
+      <p class="description project-description">
+        ${projectDescription}
+      </p>
+      <div class="project-details-pictures-gird">
+        <img
+          class="project-details-picture"
+          src="./resources/project-photos/musicare-project-image-slider.jpg"
+          alt=""
+        />
+        <img
+          class="project-details-picture"
+          src="./resources/project-photos/musicare-project-grid-photo.jpg"
+          alt=""
+        />
+        <img
+          class="project-details-picture"
+          src="./resources/project-photos/musicare-project-hero-photo.jpg"
+          alt=""
+        />
+        <img
+          class="project-details-picture"
+          src="./resources/project-photos/checkout-form.jpg"
+          alt=""
+        />
+      </div>
+      <div class="description paragraph1">
+        ${paragraph1}
+      </div>
+      <h5 class="project-subtitle chalanges">${subtitle1}</h5>
+      <p class="description paragraph2">
+        ${subtitleParagraph}
+      </p>
+      <h5 class="project-subtitle overcome">${subtitle2}</h5>
+      <p class="description paragraph3">
+        ${subtitleParagraph2}
+      </p>
+      <h5 class="project-subtitle features">Most interesting features</h5>
+      <ul class="best-features">
+        <li>Modern but elegant design</li>
+        <li>Dinamic accent color for the different sites that you choose</li>
+        <li>Split landing page</li>
+        <li>Working instrument filter</li>
+        <li>Secret quizz game for discounts</li>
+      </ul>
+    `;
+  });
+};
+///////////////
+const insertNextProjects = (arr) => {
+  let counter = 0;
   const projectsContainer = document.querySelector(
     ".projects-carousel-container"
   );
   const currentProjectName = projectNames[projectIndex];
 
-  projectsArr.forEach((project) => {
+  arr.forEach((project) => {
     let name = project.name;
     let paragraphCard = project.paragraphCard;
     let picture1 = project.picture1;
@@ -428,15 +551,45 @@ const insertNextProjects = () => {
   </div>`;
     if (currentProjectName !== name) {
       projectsContainer.innerHTML = newProjectHTML;
+      insertProjectDetails(project, counter);
     }
-    if (projectIndex >= projectsArr.length) {
+    if (projectIndex >= arr.length) {
       projectIndex = 0;
     }
   });
   animateProjectCard();
   clickRightBrn();
+  clickLeftBtn();
 
-  console.log(projectNames);
   projectIndex++;
 };
-insertNextProjects();
+insertNextProjects(projectsArr);
+// details abot projects
+(function showProjectDetails() {
+  const seeMoreBtn = document.querySelector(".see-more-btn");
+  const projectDetailsContainer = document.querySelector(
+    ".project-details-container"
+  );
+  const experienceChapterContainer = document.querySelector(
+    ".experience-chapter"
+  );
+  const linksContainer = document.querySelector(".links-container");
+
+  seeMoreBtn.addEventListener("click", () => {
+    projectDetailsContainer.style.transform = "translateX(0px)";
+    pageScroll(2000);
+    linksContainer.style.display = "none";
+    // experienceChapterContainer.style.display = "none";
+  });
+})();
+
+(function hideProjectDetails() {
+  const projectDetailsContainer = document.querySelector(
+    ".project-details-container"
+  );
+  const closeBtn = projectDetailsContainer.querySelector(".fa-angle-left");
+
+  closeBtn.addEventListener("click", () => {
+    projectDetailsContainer.style.transform = "translateX(-400%)";
+  });
+})();
